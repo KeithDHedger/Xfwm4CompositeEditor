3eb6380199f3cf858e105faa383ebd9b26e6a1e5
K D Hedger <kdhedger68713@gmail.com>
2018-07-31 14:13:57 +0100

    Updated addresses

--- Xfwm4CompositeEditor/src/main.cpp
+++ Xfwm4CompositeEditor/src/main.cpp
@@ -1,7 +1,21 @@
 /*
  *
- * K.D.Hedger 2012 <kdhedger68713@gmail.com>
- *
+ * ©K. D. Hedger. Tue 31 Jul 14:13:44 BST 2018 keithdhedger@gmail.com
+
+ * This file (main.cpp) is part of Xfwm4CompositeEditor.
+
+ * Xfwm4CompositeEditor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * Xfwm4CompositeEditor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Xfwm4CompositeEditor.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
